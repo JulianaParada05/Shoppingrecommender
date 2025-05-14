1aed0a91b24e5b19ee52e5c79d226fe092993a23
Juliana Parada Romero <jparadar@udistrital.edu.co>
2025-05-14 14:58:52 -0500

    v 0.5

--- docs/Programa.js
+++ docs/Programa.js
@@ -9,3 +9,17 @@ L.marker([4.628203153995866, -74.06589474589367])
     .addTo(map)
     .bindPopup('Universidad Distrital - Sede Calle 40')
     .openPopup();
+    
+    var marker =L.marker ([4.628203153995866, -74.06589474589367]);
+    marker.addTo(map);
+  // Abrir el archivo de GeoJSON 
+  // Javascript se ejecuta de tal manera que si una línea de código se demora, se pasa a la siguiente sin esperar.
+  // Esto puede generar problemas cuando se requiere secuencialidad 
+  
+async function cargarPuntos(){
+        var miArchivo= await fetch("microondas.geojson");
+        // Convertir el contenido a json: Objeto js
+
+        var datos= await miArchivo.json();
+}
+cargarPuntos();
