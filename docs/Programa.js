cdca5789e5be6bed274ac18c99352448d87994e6
Juliana Parada Romero <jparadar@udistrital.edu.co>
2025-05-14 15:22:50 -0500

    v 0.6

--- docs/Programa.js
+++ docs/Programa.js
@@ -15,11 +15,17 @@ L.marker([4.628203153995866, -74.06589474589367])
   // Abrir el archivo de GeoJSON 
   // Javascript se ejecuta de tal manera que si una línea de código se demora, se pasa a la siguiente sin esperar.
   // Esto puede generar problemas cuando se requiere secuencialidad 
-  
+
 async function cargarPuntos(){
         var miArchivo= await fetch("microondas.geojson");
         // Convertir el contenido a json: Objeto js
 
         var datos= await miArchivo.json();
+        console.log(datos);
+         for (let i=0; i < 10; i++) {
+            console.log(datos["features"][i]);
+            // Obtener el arreglo de la llave features
+            let listaFeatures= datos[features];
+         }
 }
 cargarPuntos();
